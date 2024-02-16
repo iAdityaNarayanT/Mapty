ddc6f0e86f66d4f57edb321026601aa59e0de03e
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-16 23:18:15 +0530

    feat calc speed function

--- script.js
+++ script.js
@@ -54,3 +54,14 @@ class Cycling extends Workout {
     this.calcSpeed();
     this._setDescription();
   }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
